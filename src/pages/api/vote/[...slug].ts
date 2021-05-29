edef28447c459c5ec89af065fe90718421d4fe23
Gabriel Freitas <gabriel.freitas@bemobi.com.br>
2021-05-29 10:11:46 -0300

    add route that return candidate votes

---
+++ src/pages/api/vote/[...slug].ts
@@ -0,0 +1,41 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import { PrismaClient } from "@prisma/client"
+
+const prisma = new PrismaClient()
+
+interface CandidatesProp {
+    id: number;
+    candidateId: number;
+    name: string;
+    countVotes: number;
+}
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+
+    var candidate = <CandidatesProp[]>[]
+
+    const { slug } = req.query
+
+    const votationId = slug[1]
+
+    try {
+        const voteResponse = await prisma.vote.findMany({
+            where: {
+                votationId: parseInt(votationId as string, 10)
+            },
+            include: {
+                candidate: true
+            },
+        })
+
+        voteResponse.map((el) => {
+            candidate.push({ id: el.id, candidateId: el.candidateId, name: el.candidate.name, countVotes: el.votesCount })
+        })
+
+        return res.status(201).json({ votesCount: candidate })
+
+    } catch (error) {
+        return res.status(error.status).json({ message: 'Fail on get candidates votesCount.' })
+    }
+
+}
