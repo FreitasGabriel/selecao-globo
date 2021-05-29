954a5a33db87f1aa07c687f83704c5928cace94e
Gabriel Freitas <gabriel.freitas@bemobi.com.br>
2021-05-29 11:37:16 -0300

    remove strong relationship between entities

--- prisma/migrations/20210529141823_init_database/migration.sql
+++ prisma/migrations/20210529141823_init_database/migration.sql
@@ -29,6 +29,3 @@ CREATE UNIQUE INDEX "Vote_candidateId_unique" ON "Vote"("candidateId");
 
 -- AddForeignKey
 ALTER TABLE "Vote" ADD FOREIGN KEY ("candidateId") REFERENCES "Candidate"("id") ON DELETE CASCADE ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "Vote" ADD FOREIGN KEY ("votationId") REFERENCES "Votation"("id") ON DELETE CASCADE ON UPDATE CASCADE;
