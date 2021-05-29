3036c61615f7dd522f7ece8079a8f6002dd5459b
Gabriel Freitas <gabriel.freitas@bemobi.com.br>
2021-05-29 10:10:43 -0300

    get route that return total os votes

---
+++ src/pages/api/votation/[...slug].ts
@@ -0,0 +1,37 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import { PrismaClient } from "@prisma/client"
+
+const prisma = new PrismaClient()
+
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+
+    const { slug } = req.query
+
+    const votationId = slug[1]
+
+    try {
+
+        const vote = await prisma.vote.findMany({
+            where: {
+                votationId: parseInt(votationId as string, 10)
+            }
+        })
+
+        let arrVotes = <number[]>[]
+
+        vote.map((el) => {
+            arrVotes.push(el.votesCount)
+        })
+
+        const total = arrVotes.reduce((acc, currentValue) => {
+            return acc + currentValue
+        })
+
+        return res.status(201).json({ totalVotes: total })
+
+    } catch (error) {
+        return res.status(error.status).json({ message: 'Error on get total of votes' })
+    }
+
+}
